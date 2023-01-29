7b6c9f1ee0820bb1ce3ae3d46ed0fd6260862934
BartlomiejGd <bartlomiejgd@gmail.com>
2023-01-29 16:15:37 +0100

    boilerplate

--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -1,14 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
+import {UserService} from "./user.service";
+import {register} from "tsconfig-paths";
 
 describe('UserController', () => {
   let controller: UserController;
 
+  const mockUserService = {
+    registerNewUser: jest.fn(registerNewUser => {
+    return{
+      emailIsOk: true,
+      nickIsOk: true,
+      pwdMatch: true
+    }
+  })
+  }
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
-
-    }).compile();
+      providers: [UserService]
+    }).overrideProvider(UserService).useValue(mockUserService).compile();
 
     controller = module.get<UserController>(UserController);
   });
@@ -16,4 +28,11 @@ describe('UserController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  it('should register/create new user', () => {
+    expect(controller.register({nick: 'JestNick', email: 'a@gmail.com' , pwd:'1234' , repwd: '1234'}))
+        .toContain({
+        })
+  })
+
 });
