e25d1da8fd9daab27ff3b72f144dc51aec1935da
BartlomiejGd <bartlomiejgd@gmail.com>
2023-02-04 20:32:41 +0100

    finish tests

--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -3,48 +3,59 @@ import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { EmailService } from '../emailConfirmation/email.service';
 import { EmailModule } from '../emailConfirmation/email.module';
-import {Repository} from "typeorm";
-import {getRepositoryToken, InjectRepository} from "@nestjs/typeorm";
-import { User } from "./user.entity";
-
+import { Repository } from 'typeorm';
+import { getRepositoryToken, InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { hashPwd } from '../utility/hash';
+import { raw } from "express";
 
 describe('UserController', () => {
   let controller: UserController;
-  let userService: UserService;
-  let userRepository: Repository<User>;
-
-  const USER_REPOSITORY_TOKEN = getRepositoryToken(User)
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
       providers: [
-        UserService,
-        EmailService,
         {
-          provide: USER_REPOSITORY_TOKEN,
+          /**
+           * Mock the UserService to return values we are expecting.
+           *
+           * This may not seem like a big deal with such a simple application
+           * especially as everything is done in memory, but this becomes even
+           * more important as services depend on other services such as TypeORM/Mongo
+           * ElasticSearch, etc.
+           */
+          provide: UserService,
           useValue: {
-            create: jest.fn(),
-            save: jest.fn(),
-            findOne: jest.fn(),
-            exist: jest.fn()
-          },
-        },
+            registerNewUser: jest
+              .fn()
+              .mockResolvedValue({emailIsOk: true, nickIsOK: true, pwdMatch: true, accountCreated: true})
+          }
+        }
       ],
     }).compile();
 
     controller = module.get<UserController>(UserController);
-    userService = module.get<UserService>(UserService);
-    userRepository = module.get<Repository<User>>(USER_REPOSITORY_TOKEN);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
 
-  it('should be defined', ()=> {
-    expect(userRepository).toBeDefined();
+  //Its important to use async and await! all my functions are async
+  it('should register user', async ()=>{
+    const retRegisterUser = await controller.register({
+      nick: 'nickTets',
+      email: 'jest@gmail.com',
+      pwd: '11111',
+      repwd:'11111'
+    })
+    expect(retRegisterUser).toMatchObject({
+      emailIsOk: true,
+      nickIsOK: true,
+      pwdMatch: true,
+      accountCreated: true
+    })
   })
-
 
 });
