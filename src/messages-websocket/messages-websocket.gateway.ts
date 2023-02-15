96393bea3100a8c7214b7b67ee1588231b02ce96
BartlomiejGd <bartlomiejgd@gmail.com>
2023-02-15 19:12:57 +0100

    preparation cn

--- src/messages-websocket/messages-websocket.gateway.ts
+++ src/messages-websocket/messages-websocket.gateway.ts
@@ -2,24 +2,35 @@ import {
   WebSocketGateway,
   SubscribeMessage,
   MessageBody,
+  WebSocketServer,
+  ConnectedSocket,
 } from '@nestjs/websockets';
 import { MessagesWebsocketService } from './messages-websocket.service';
 import { CreateMessagesWebsocketDto } from './dto/create-messages-websocket.dto';
 import { UpdateMessagesWebsocketDto } from './dto/update-messages-websocket.dto';
+import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway({
   cors: { origin: '*' },
 })
 export class MessagesWebsocketGateway {
+  @WebSocketServer()
+  server: Server;
+
   constructor(
     private readonly messagesWebsocketService: MessagesWebsocketService,
   ) {}
 
   @SubscribeMessage('createMessagesWebsocket')
-  create(
+  async screate(
     @MessageBody() createMessagesWebsocketDto: CreateMessagesWebsocketDto,
   ) {
-    return this.messagesWebsocketService.create(createMessagesWebsocketDto);
+    const message = await this.messagesWebsocketService.create(
+      createMessagesWebsocketDto,
+    );
+
+    this.server.emit('message', message);
+    return message;
   }
 
   @SubscribeMessage('findAllMessagesWebsocket')
@@ -28,12 +39,16 @@ export class MessagesWebsocketGateway {
   }
 
   @SubscribeMessage('typing')
-  async typing() {
-    //todo
+  async typing(
+    @MessageBody('isTyping') isTyping: boolean,
+    @ConnectedSocket() client: Socket,
+  ) {
+    const name = await this.messagesWebsocketService.getClientName(client.id);
+    client.broadcast.emit('typing', { name, isTyping });
   }
 
   @SubscribeMessage('join')
-  join() {
-    //todo
+  join(@MessageBody('name') name: string, @ConnectedSocket() client: Socket) {
+    return this.messagesWebsocketService.identify(name, client.id);
   }
 }
--- src/messages-websocket/messages-websocket.service.ts
+++ src/messages-websocket/messages-websocket.service.ts
@@ -6,8 +6,21 @@ import { MessagesWebsocket } from './entities/messages-websocket.entity';
 @Injectable()
 export class MessagesWebsocketService {
   messages: MessagesWebsocket[] = [{ name: 'Tom', text: 'heyy Boo' }];
+  clientToUser = {};
+
+  identify(name: string, clientId: string) {
+    this.clientToUser[clientId] = name;
+
+    return Object.values(this.clientToUser);
+  }
+
+  getClientName(clientId: string) {
+    return this.clientToUser[clientId];
+  }
+
   create(createMessagesWebsocketDto: CreateMessagesWebsocketDto) {
-    return this.messages.push(createMessagesWebsocketDto);
+    const message = { ...createMessagesWebsocketDto };
+    return this.messages.push(message);
   }
 
   findAll() {
